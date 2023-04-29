f144b131909f2be7a21f41361e6e8719904ffd46
Jacquwes <38167139+Jacquwes@users.noreply.github.com>
2023-04-29 19:06:25 +0200

    Update main function

--- Kernel/kernel/src/kernel.cpp
+++ Kernel/kernel/src/kernel.cpp
@@ -1,14 +1,32 @@
 #include <cstdint>
-
+#include <cstdio>
+#include <memory>
+#include <new>
 #include <string>
 
-class chart : public std::char_traits<char>
-{
+#include <kernel.h>
+#include <memory_manager.h>
+#include <multiboot.h>
+#include <output.h>
 
-};
+#define LOG
 
-void main()
+void main(multiboot_info_t* info, uint32_t magic)
 {
-	volatile int a = 3;
-	chart b[5];
+	kernel::output();
+	auto memory = kernel::memory_manager(info, magic);
+
+	if (memory.get_error_code() != kernel::SUCCESS)
+	{
+		std::printf("kernel: Failed initializing memory manager. %x\n", memory.get_error_code());
+		return;
+	}
+
+	char* arr = new char[64];
+	
+	memory.display_pages();
+	
+	delete[] arr;
+
+	memory.display_pages();
 }
