aff0377ecd10b1f3939b45c3f7726430adf8617c
Jacquwes <38167139+Jacquwes@users.noreply.github.com>
2023-04-30 19:22:32 +0200

    Add regions and implement +=

--- Kernel/stl/include/basic_string.h
+++ Kernel/stl/include/basic_string.h
@@ -27,7 +27,8 @@ namespace std
 		using iterator			= default_iterator<pointer>;
 		using const_iterator	= default_iterator<const_pointer>;
 
-		constexpr basic_string() noexcept(noexcept(allocator_type()))
+#pragma region Members
+		inline constexpr basic_string() noexcept(noexcept(allocator_type()))
 			: basic_string(allocator_type()) {}
 
 		inline constexpr basic_string(allocator_type const& alloc) noexcept
@@ -54,7 +55,7 @@ namespace std
 			_data[count] = 0;
 		}
 
-		inline constexpr basic_string(basic_string const& other)
+		inline constexpr basic_string(basic_string const& other)			
 		{
 			_alloc = other._alloc;
 			_data = other._data;
@@ -68,12 +69,52 @@ namespace std
 
 			for (_size = _capacity = 0; string[_size]; _size++, _capacity++);
 
-			_data = alloc.allocate(_size + 1);
+			_data = _alloc.allocate(_size + 1);
 			memcpy(_data, string, _size);
 			_data[_size + 1] = 0;
 		}
 
+		inline constexpr allocator_type get_allocator() const noexcept { return _alloc; }
+#pragma endregion
+
+#pragma region Operators
+	public:
+		inline constexpr basic_string& operator+=(const char* other)
+		{
+			size_type other_size = 0;
+			for (size_type i = 0; other[i]; ++i, ++other_size);
+
+			if (_size + other_size <= _capacity)
+			{
+				for (size_type i = 0; i < other_size; ++i)
+				{
+					_data[_size + i] = other[i];
+				}
+
+				_size += other_size;
+				_data[_size] = 0;
+
+				return *this;
+			}
+
+			pointer new_data = _alloc.allocate(_size + other_size + 1);
+			memcpy(new_data, _data, _size);
+			memcpy(new_data + _size, other, other_size);
+			new_data[_size + other_size] = 0;
+
+			_alloc.deallocate(_data, _capacity);
+			_data = new_data;
+			_capacity = _size + other_size;
+			_size = _capacity;
+
+			return *this;
+		}
+#pragma endregion
+
+#pragma region Element Access
+	public:
 		inline constexpr value_type* c_str() const noexcept { return _data; }
+#pragma endregion
 
 	private:
 		Allocator _alloc;
