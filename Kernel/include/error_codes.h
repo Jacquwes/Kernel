e6bba09f719fa56440bd1a7dff220902a9677f8c
Jacquwes <38167139+Jacquwes@users.noreply.github.com>
2023-05-16 22:43:40 +0200

    Move error_codes to new file

---
+++ Kernel/include/error_codes.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <cstdint>
+
+namespace kernel
+{
+	typedef uint32_t error_code;
+
+	constexpr error_code SUCCESS = 0x00000000;
+}
--- Kernel/kernel/include/kernel.h
+++ Kernel/kernel/include/kernel.h
@@ -1,16 +1,3 @@
 #pragma once
 
 #include <cstdint>
-
-namespace kernel
-{
-	typedef uint32_t error_code;
-
-	constexpr error_code SUCCESS = 0x00000000;
-
-	inline void print(char c, int x = 0, int y = 0)
-	{
-		uint16_t* videoMemory = (uint16_t*)0xb8000;
-		videoMemory[(y * 80) + x] = c | (15 << 8);
-	}
-}
