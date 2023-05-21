b258e32f542e0916f4de840cdea88f3abb0877e6
Jacquwes <38167139+Jacquwes@users.noreply.github.com>
2023-05-21 14:34:18 +0200

    Test vector

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -9,15 +9,25 @@ static inline bool are_interrupts_enabled()
 	return flags & (1 << 9);
 }
 
+#include <vector>
+
 void main(multiboot_info_t* info, uint32_t magic)
 {
-	kernel::logger::set_canals(kernel::log_canal::info | kernel::log_canal::error | kernel::log_canal::warning | kernel::log_canal::success);
+	kernel::logger::set_canals(kernel::log_canal::all);
 	kernel::output();
 	kernel::memory_manager(info, magic);
 	kernel::global_descriptor_table();
 	kernel::interrupt_descriptor_table();
 	kernel::keyboard_manager();
 	kernel::pci_manager();
+
+	std::vector<char> test;
+	test.push_back('t');
+	test.push_back('e');
+	test.push_back('s');
+	test.push_back('t');
+
+	std::printf("vector.at(2) = %c\n", test.at(2));
 
 	for (;;)
 		__asm__ volatile ("hlt");
