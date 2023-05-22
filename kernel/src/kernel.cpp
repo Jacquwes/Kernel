1438646f319ea8d0561751bda4ecdb0ac8c7e94d
Jacquwes <38167139+Jacquwes@users.noreply.github.com>
2023-05-22 18:54:15 +0200

    More vector tests

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -16,19 +16,34 @@ void main(multiboot_info_t* info, uint32_t magic)
 	kernel::logger::set_canals(kernel::log_canal::all);
 	kernel::vga_output();
 	kernel::serial_output();
-	kernel::memory_manager(info, magic);
+	auto mem = kernel::memory_manager(info, magic);
+	mem.display_pages();
 	kernel::global_descriptor_table();
 	kernel::interrupt_descriptor_table();
 	kernel::keyboard_manager();
 	kernel::pci_manager();
 
-	std::vector<char> test;
-	test.push_back('t');
-	test.push_back('e');
-	test.push_back('s');
-	test.push_back('t');
+	std::vector<char> v;
 
-	std::printf("vector.at(2) = %c\n", test.at(2));
+	v.push_back('1');
+	v.push_back('2');
+	v.push_back('3');
+	v.push_back('4');
+
+	std::vector<char> v2 = v;
+
+	v2.push_back('5');
+	v2.push_back('6');
+	v2.push_back('7');
+	v2.push_back('8');
+
+	for (int i = 0; i < 4; i++)
+		std::printf("Vector > Element %d: %c\n", i, v.at(i));
+
+	for (int i = 0; i < 4; i++)
+		std::printf("Vector > Element %d: %c\n", i, v2.at(i));
+
+	mem.display_pages();
 
 	for (;;)
 		__asm__ volatile ("hlt");
